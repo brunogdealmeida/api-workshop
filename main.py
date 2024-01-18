61d5a94d9d85d31121d013707ca8487fa24ca0a7
Bruno Gomes de Almeida - BGO <bruno.gomes@iteris.com.br>
2024-01-18 14:13:48 -0300

    Adicionando primeiros arquivos do projeto

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from typing import List, Dict
+
+produtos: List[Dict[str, any]] = [
+    {
+        "id": 1,
+        "nome":"Notebook",
+        "preco": 15.00
+    },
+    {
+        "id": 2,
+        "nome":"Celular",
+        "preco": 10.00
+    }
+
+]
+
+app = FastAPI()
+
+@app.get("/") #Recebe requisições Get
+def hello_world():
+    return {"Hello": "World"}
+
+@app.get("/produtos")
+def list_products():
+    return produtos
---
+++ tests.py
@@ -0,0 +1,22 @@
+import pytest
+
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_hello_world():
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_hello_world_json():
+    response = client.get("/")
+    assert response.json() == {"Hello": "World"}
+
+def test_list_products():
+    response = client.get("/produtos")
+    assert response.status_code == 200
+
+def test_lenght_list_products():
+    response = client.get("/produtos")
+    assert len(response.json()) == 2
